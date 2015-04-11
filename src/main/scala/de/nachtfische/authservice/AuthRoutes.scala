183c33eb894b2efad1e753c19136206dd6c24084
christian <christian.fischer@innoveo.com>
2015-04-11 15:00:33 +0200

    added hashing, validation and retrieval of accounts

--- src/main/scala/de/nachtfische/authservice/AuthRoutes.scala
+++ src/main/scala/de/nachtfische/authservice/AuthRoutes.scala
@@ -1,7 +1,8 @@
 package de.nachtfische.authservice
 
 import java.nio.charset.StandardCharsets
-import java.util.Base64
+import java.security.MessageDigest
+import java.util.{UUID, Base64}
 
 import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl
 import spray.http._
@@ -10,6 +11,7 @@ import spray.httpx.SprayJsonSupport._
 import spray.json._
 import spray.routing.HttpService
 
+import scala.Predef
 import scala.collection.JavaConversions
 import scala.collection.mutable.ListBuffer
 
@@ -19,18 +21,20 @@ case class CreateAccountResponse(href:String)
 case class LoginAttemptsRequest(value:String)
 case class ErrorResponse(message:String)
 
-case class Account(id:String, email:String, password:String)
+case class Credentials(email:String, password:String)
+case class Account(id:String, email:String)
 
 case class GoogleProvider(clientId:String, clientSecret:String, redirectUri:String)
 
 trait AuthenticationRoutes extends HttpService with DefaultJsonProtocol {
 
-  implicit val accountFormat = jsonFormat2(CreateAccountRequest)
-  implicit val accountCreatedResponseFormat = jsonFormat1(CreateAccountResponse)
+  implicit val accountFormat = jsonFormat2(Account)
+  implicit val createAccountRequestFormat = jsonFormat2(CreateAccountRequest)
+  implicit val createAccountResponseFormat = jsonFormat1(CreateAccountResponse)
   implicit val loginAttemptsRequestFormat = jsonFormat1(LoginAttemptsRequest)
   implicit val errorResponseFormat = jsonFormat1(ErrorResponse)
 
-  val accountService:ListBuffer[Account] = ListBuffer()
+  val accountService:AccountService
 
   private val clientID: String = sys.env.getOrElse("GOOGLE_CLIENT_ID", "clientID")
   private val clientSecret: String = sys.env.getOrElse("GOOGLE_CLIENT_SECRET", "clientSecret")
@@ -40,16 +44,14 @@ trait AuthenticationRoutes extends HttpService with DefaultJsonProtocol {
     pathPrefix("v1" / "accounts") {
       post {
         entity(as[CreateAccountRequest]) { createAccountRequest =>
-          val account = Account(java.util.UUID.randomUUID.toString, createAccountRequest.email, createAccountRequest.password)
-          accountService.+=(account)
-
+          val account = accountService.create(Credentials(createAccountRequest.email, createAccountRequest.password))
           complete(CreateAccountResponse(s"/v1/accounts/${account.id}"))
         }
       } ~ path(Segment) { id =>
         get {
-          val find = accountService.find(_.id == id)
+          val find = accountService.get(id)
           find match {
-            case Some(account) => complete(CreateAccountResponse("some"))
+            case Some(account) => complete(account)
             case None => complete(HttpResponse(404))
           }
 
@@ -96,4 +98,44 @@ trait AuthenticationRoutes extends HttpService with DefaultJsonProtocol {
   def decodeBase64Utf8(some: String): String = {
     new Predef.String(Base64.getDecoder.decode(some), StandardCharsets.UTF_8)
   }
+}
+
+trait AccountService {
+  def create(credentials: Credentials): Account
+  def authenticate(account: Credentials): Option[Account]
+  def get(id:String): Option[Account]
+}
+
+trait InMemoryAccountService extends AccountService {
+
+  private val toAccount: (AccountRecord) => Account = a => Account(a.id, a.email)
+  private val accounts = ListBuffer[AccountRecord]()
+
+  def create(credentials: Credentials): Account = {
+    accounts += AccountRecord(generateId, credentials.email, md5(credentials.password))
+
+    Account(generateId, credentials.email)
+  }
+
+  def authenticate(credentials: Credentials): Option[Account] = {
+    val hash = md5(credentials.password)
+    accounts
+      .find(a => a.hashedPassword == hash && a.email == credentials.email)
+      .map(toAccount)
+  }
+
+  def get(id:String) = {
+    accounts.find(a => a.id == id).map(toAccount)
+  }
+
+  private def generateId: String = {
+    UUID.randomUUID.toString
+  }
+
+  private def md5(password: String): String = {
+    new Predef.String(MessageDigest.getInstance("MD5").digest(password.getBytes))
+  }
+
+  case class AccountRecord(id:String, email:String, hashedPassword:String)
+
 }
